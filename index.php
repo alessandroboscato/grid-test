0f7ee6c019d72d0025026b47b290ef999b467d2b
Alessandro <alessandro.boscato@gmail.com>
2021-01-07 15:40:12 +0100

    php for cycle

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -30,165 +32,15 @@
         <img class="look" src="img/5419_1.jpg" alt="">
       </div>
 
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 1</p>
-        </div>
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 2</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 3</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 4</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 5</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 6</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 7</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 8</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 9</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 10</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 11</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 12</p>
-        </div>
-      </div>
-
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 13</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 14</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 15</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 16</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 17</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 18</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 19</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 20</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 21</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 22</p>
-        </div>
-      </div>
-      <div class="item">
-        <img src="img/k013_ai20_1.jpg" alt="">
-        <div class="product-details">
-          <h4>Jeans cotone</h4>
-          <p>ABITO IN TESSUTO 23</p>
-        </div>
-      </div>
+      <?php for ($i = 0; $i < 20; $i++){ ?>
+          <div class="item">
+            <img src="img/k013_ai20_1.jpg" alt="">
+            <div class="product-details">
+              <h4>Jeans cotone</h4>
+              <p>ABITO IN TESSUTO <?php echo $i ?></p>
+            </div>
+          </div>
+        <?php } ?>
 
     </div>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
