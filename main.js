3f70ae5b72d88a4538663117b70f90e65ca3b449
Alessandro <alessandro.boscato@gmail.com>
2021-01-07 15:30:35 +0100

    changed wv & wh

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
 
   var range = 1770;
 //for getting beginning values
-var windowW = $(window).height();
-var windowH = $(window).width();
+var windowH = $(window).height();
+var windowW = $(window).width();
 console.log(windowW+'X'+windowH);
 
 //for getting current values
 // $(window).on('resize', function(){
 $(window).resize(function(){
-   var windowW = $(this).height();
-   var windowH = $(this).width();
+   var windowH = $(this).height();
+   var windowW = $(this).width();
    console.log(windowW+'X'+windowH);
    if( windowH > range ) {
       $('.special-item').show();
