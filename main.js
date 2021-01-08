5948b93b99f38f879ef190ffc732aa3ca223bc4b
Alessandro <alessandro.boscato@gmail.com>
2021-01-08 12:24:15 +0100

    add jquery

--- main.js
+++ main.js
@@ -9,6 +9,26 @@ var container = $(".grid").height();
 console.log('width: '+windowW+' height: '+windowH);
 console.log(container);
 
+if (windowW < 1769) {
+  if (container > 1990) {
+    $('.special-item-1').show();
+    if (container > 3990) {
+      $('.special-item-2').show();
+      if (container > 5990) {
+        $('.special-item-3').show();
+        if (container > 7990) {
+          $('.special-item-4').show();
+          if (container > 11990) {
+            $('.special-item-5').show();
+          }
+        }
+      }
+    }
+  }
+} else {
+
+}
+
 //for getting current values
 // $(window).on('resize', function(){
 $(window).resize(function(){
