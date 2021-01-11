b0190c8dc148efbd230018e9b99f62591b29ed0c
Alessandro <alessandro.boscato@gmail.com>
2021-01-11 15:22:02 +0100

    not fixed yet

--- .main.js
+++ .main.js
@@ -8,7 +8,7 @@ console.log(container);
 
   // intructions to display looks depending on height grid container
 
-  if (windowW > 930) {
+  if (windowW > 930 && windowW < 1140) {
 
       if (container < 1490) {
         $('.look-box.special-item-0').show();
@@ -34,38 +34,38 @@ console.log(container);
       if (container > 7490) {
         $('.look-box.special-item-5').show();
       };
-  }
+  } else if (windowW > 1140 && windowW < 1350) {
 
+    if (container < 1490) {
+      $('.look-box.special-item-0').show();
+    };
+
+    if (container > 1490) {
+      $('.look-box.special-item-0').show();
+      $('.look-box.special-item-1').show();
+    };
+
+    if (container > 3490) {
+      $('.look-box.special-item-2').show();
+    };
+
+    if (container > 4490) {
+      $('.look-box.special-item-3').show();
+    };
+
+    if (container > 5990) {
+      $('.look-box.special-item-4').show();
+    };
+
+    if (container > 7490) {
+      $('.look-box.special-item-5').show();
+    };
+  };
+
+  var windowW = $(window).width();
   var container = $(".grid").height();
+  console.log(windowW);
   console.log(container);
-
-  if (windowW > 930) {
-
-      if (container < 1490) {
-        $('.look-box.special-item-0').show();
-      };
-
-      if (container > 1490) {
-        $('.look-box.special-item-0').show();
-        $('.look-box.special-item-1').show();
-      };
-
-      if (container > 2990) {
-        $('.look-box.special-item-2').show();
-      };
-
-      if (container > 4490) {
-        $('.look-box.special-item-3').show();
-      };
-
-      if (container > 5990) {
-        $('.look-box.special-item-4').show();
-      };
-
-      if (container > 7490) {
-        $('.look-box.special-item-5').show();
-      };
-  }
 
 
 
